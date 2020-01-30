92dcafc4739cae19b9e4716c6a0df697897140bd
Zigmars Vilcans <zims@inbox.lv>
2020-01-30 15:00:09 +0200

    futureBuilder

--- lib/MessageList.dart
+++ lib/MessageList.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'Message.dart';
 
 class MessageList extends StatefulWidget {
@@ -14,59 +13,75 @@ class MessageList extends StatefulWidget {
 }
 
 class _MessageListState extends State<MessageList> {
-  List<Message> messages = const [];
-
-  Future loadMessageList() async {
-    http.Response response =
-        await http.get('http://www.mocky.io/v2/5e32c60f3200006e0094d0e8');
-    String content = response.body;
-    List collection = json.decode(content);
-    List<Message> _messages =
-        collection.map((json) => Message.fromJson(json)).toList();
-
-    setState(() {
-      messages = _messages;
-    });
-  }
+  Future<List<Message>> messages;
 
   @override
   void initState() {
-    loadMessageList();
     super.initState();
+    messages = Message.browse();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: ListView.separated(
-        itemCount: messages.length,
-        separatorBuilder: (context, index) => Divider(),
-        itemBuilder: (BuildContext context, int index) {
-          Message message = messages[index];
-          return ListTile(
-            title: Text(message.subject),
-            isThreeLine: true,
-            leading: CircleAvatar(
-              backgroundColor: Colors.blueAccent,
-              child: Text(
-                'A',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.yellow,
-                ),
-              ),
-            ),
-            subtitle: Text(
-              message.body,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-          );
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                var _messages = Message.browse();
+
+                setState(() {
+                  messages = _messages;
+                });
+              },
+            )
+          ],
+        ),
+        body: FutureBuilder(
+          future: messages,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+              case ConnectionState.active:
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              case ConnectionState.done:
+                if (snapshot.hasError)
+                  return Text("there was an error: ${snapshot.error}");
+                var messages = snapshot.data;
+
+                return ListView.separated(
+                  itemCount: messages.length,
+                  separatorBuilder: (context, index) => Divider(),
+                  itemBuilder: (BuildContext context, int index) {
+                    Message message = messages[index];
+                    return ListTile(
+                      title: Text(message.subject),
+                      isThreeLine: true,
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.blueAccent,
+                        child: Text(
+                          'A',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.yellow,
+                          ),
+                        ),
+                      ),
+                      subtitle: Text(
+                        message.body,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    );
+                  },
+                );
+            }
+          },
+        ));
   }
 }
